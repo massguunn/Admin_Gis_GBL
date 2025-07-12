cf9b2f3790b77bab67769abe87fdceb60ee8b840
massguunn <rohadigunawan0@gmail.com>
2025-07-12 10:58:37 +0800

    mengganti kode database biar online

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,7 +1,17 @@
+// UNTUK LOCAL
+// module.exports = {
+//   multipleStatements: true,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "gb_db",
+// };
+
+// UNTUK ONLINE
 module.exports = {
   multipleStatements: true,
   host: "localhost",
-  user: "root",
-  password: "",
+  user: "gb_db",
+  password: "Tb8ydMMcBiArszcr",
   database: "gb_db",
 };
